61ea0f45699cccc65d3d54feb3fa29e8101feec7
67TH1PhamMinhTuan0101666 <tunphamdz1905@gmail.com>
2025-04-05 20:32:19 +0700

    Add files via upload

---
+++ 23_01_API/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_01_API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Day la nhanh 2");
+           
+        }
+    }
+}
