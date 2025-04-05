f6c7427ce8fcacf6782f049956c1f80e63750532
67TH1PhamMinhTuan0101666 <tunphamdz1905@gmail.com>
2025-04-05 20:42:47 +0700

    Add files via upload

---
+++ ltbbai2/bt2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("nhap so b::");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("nhap so c:");
+            double c = double.Parse(Console.ReadLine());
+            // tinh Delta
+            double delta, x1, x2, x;
+            delta = b * b - 4 * a * c;
+            if (delta < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            if (delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(delta) / (2 * a));
+                x2 = (-b - Math.Sqrt(delta) / (2 * a));
+                Console.WriteLine(" Nhap gia tri nghiem x1, x2:" + x1 + x2);
+                Console.ReadLine();
+
+            }
+            if (delta == 0)
+            {
+                x= (-b /(2 * a));
+                Console.WriteLine(" Nhap gia tri nghiem x:" +x);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ ltbbai2/ltbim/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ltbim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("nhap so b::");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("nhap so c");
+            double c = double.Parse(Console.ReadLine());
+            double min = 0;
+            if (a > b)
+            { min = b; }
+            else { min = a; }
+
+            if (min > c)
+            { min = c; }
+            Console.Write("So nho nhat la" + min);
+            Console.ReadLine();
+        }
+    }
+}
