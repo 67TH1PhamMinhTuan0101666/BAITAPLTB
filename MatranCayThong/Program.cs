6948998915909b6807591590051ab5a1660fa7c9
67TH1PhamMinhTuan0101666 <tunphamdz1905@gmail.com>
2025-04-05 20:35:41 +0700

    Add files via upload

---
+++ MatranCayThong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatranCayThong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap chieu cao n: ");
+            n= Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
