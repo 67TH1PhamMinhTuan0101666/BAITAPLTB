4d85cafb322a1b27a20ee474119aa8a8c2a8f937
67TH1PhamMinhTuan0101666 <tunphamdz1905@gmail.com>
2025-04-05 20:34:58 +0700

    Add files via upload

---
+++ chuvihinhtron/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chuvihinhtron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R;
+            Console.Write("Nhap vao ban kinh duong tron: ");
+            R=Convert.ToDouble(Console.ReadLine());
+            tinhchuvihinhtron(R);
+            Console.ReadLine();
+        }
+        private static double tinhchuvihinhtron(double radius)
+        {
+            double pi = 3.1415926;
+            double chuvi=2*pi*radius;
+            Console.WriteLine("Ban kinh duong tron: " + radius);
+            Console.WriteLine("Chu vi duong tron: "+chuvi);
+            return chuvi;
+        }
+        private  static double tinhdientich(double radius)
+        {
+            double pi = 3.1415926;
+            double S = (pi * 2 * radius * 2 * radius) / 4;
+            Console.WriteLine("Dien tich hinh tron la: ");
+            Console.ReadLine() ;
+            return S;
+        }
+    }
+}
