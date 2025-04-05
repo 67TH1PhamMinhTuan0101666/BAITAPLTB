65182440f49c7570d06b78d5079a72d2c2b380bc
67TH1PhamMinhTuan0101666 <tunphamdz1905@gmail.com>
2025-04-05 20:40:01 +0700

    Add files via upload

---
+++ ThongtinSinhvien/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThongtinSinhvien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinhvien Thongtin = new Sinhvien("Nam","BacGiang",0098266,08,09,07);
+            Console.Write("Thong tin sinh vien la: " + Thongtin.Hoten + " " + Thongtin.Quequan + " " + Thongtin.MSSV + " " +Thongtin.diemToan+" "
+                +Thongtin.diemLy+" "+Thongtin.diemHoa);
+            Console.ReadLine();
+            double diemTB;
+            diemTB = (Thongtin.diemToan + Thongtin.diemLy + Thongtin.diemHoa) / 3;
+            Console.Write("Diem TB la: " + diemTB);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ThongtinSinhvien/Sinhvien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThongtinSinhvien
+{
+    public class Sinhvien
+    {//Thuoc tinh         
+        public string Hoten;
+        public string Quequan;
+        public int MSSV;
+        public int diemToan;
+        public int diemLy;
+        public int diemHoa;
+        //Phuong thuc
+
+        public Sinhvien(string _Hoten, string _Quequan, int _MSSV, int _diemToan, int _diemLy, int _diemHoa)
+        {
+            this.Hoten = _Hoten;
+            this.Quequan = _Quequan;
+            this.MSSV = _MSSV;
+            this.diemToan= _diemToan;
+            this.diemLy = _diemLy;
+            this.diemHoa = _diemHoa;
+            
+        }
+    }
+}
