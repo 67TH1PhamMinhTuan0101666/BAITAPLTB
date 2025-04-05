fa6ed9df382cb142c3f82b1ab8274cdb99a9525f
67TH1PhamMinhTuan0101666 <tunphamdz1905@gmail.com>
2025-04-05 20:37:45 +0700

    Add files via upload

---
+++ ThucHanhClass_20_02/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanhClass_20_02
+{
+    public class Person
+    {
+        private string Name;
+        public int height;
+        public int weight;
+        public bool ismale;
+        //Ham khoi tao
+        public Person( string _Name,int _height,int _weight, bool _ismale )
+        {
+            this.Name = _Name;
+            this.height = _height;
+            this.weight = _weight;
+            this.ismale = _ismale;
+        }
+        public string getName() 
+        { 
+            string Ten=this.Name;
+            return Ten;
+        }
+    }
+}
---
+++ ThucHanhClass_20_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanhClass_20_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Name = {"Nam","Tan","Nien","Linh","Hoang" };
+            int[] height = { 176, 154, 181, 167, 170 };
+            int[] weight = {85, 60, 75, 60, 70 };
+            bool[] gender = { true, true, true, false, true };
+
+            Person person1= new Person("Tien",165,81,true);
+            Console.Write("Thong tin person: " + person1.getName());
+            Console.ReadLine();
+        }
+    }
+}
